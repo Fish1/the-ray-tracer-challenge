fa34d5369feeb170e4afbe0ced0579ebe255fc9b
Jacob Enders <jacobenders1@gmail.com>
2023-04-30 08:41:41 -0400

    convert math to operator overloads

--- src/math/tuple.rs
+++ src/math/tuple.rs
@@ -7,90 +7,103 @@ pub struct Tuple {
   pub w: f64,
 }
 
+impl std::ops::Add for Tuple {
+    type Output = Tuple;
+    fn add(self, rhs: Tuple) -> Self::Output {
+        Tuple {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+            w: self.w + rhs.w,
+        }
+    }
+}
+
+impl std::ops::Sub for Tuple {
+    type Output = Tuple;
+    fn sub(self, rhs: Tuple) -> Self::Output {
+        Tuple {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+            w: self.w - rhs.w,
+        }
+    }
+}
+
+impl std::ops::MulAssign<f64> for Tuple {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+        self.w *= rhs;
+    }
+}
+
+impl std::ops::DivAssign<f64> for Tuple {
+    fn div_assign(&mut self, rhs: f64) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+        self.w /= rhs;
+    }
+}
+
+impl std::ops::Neg for Tuple {
+    type Output = Tuple; 
+    fn neg(self) -> Self::Output {
+        Tuple {
+            x: self.x * -1.0,
+            y: self.y * -1.0,
+            z: self.z * -1.0,
+            w: self.w * -1.0,
+        }
+    }
+}
+
+
 impl Tuple {
-  pub fn new_vector(x: f64, y: f64, z: f64) -> Tuple {
-    Tuple {
-      x, y, z, w: 0.0
+    pub fn new_vector(x: f64, y: f64, z: f64) -> Tuple {
+        Tuple {
+          x, y, z, w: 0.0
+        }
     }
-  }
 
-  pub fn new_point(x: f64, y: f64, z: f64) -> Tuple {
-    Tuple {
-      x, y, z, w: 1.0
+    pub fn new_point(x: f64, y: f64, z: f64) -> Tuple {
+        Tuple {
+          x, y, z, w: 1.0
+        }
     }
-  }
 
-  pub fn new_tuple(x: f64, y: f64, z: f64, w: f64) -> Tuple {
-    Tuple {
-      x, y, z, w
+    pub fn new_tuple(x: f64, y: f64, z: f64, w: f64) -> Tuple {
+        Tuple {
+          x, y, z, w
+        }
     }
-  }
 
-  pub fn is_vector(&self) -> bool {
-    compare::equal(self.w, 0.0)
-  }
-
-  pub fn is_point(&self) -> bool {
-    compare::equal(self.w, 1.0)
-  }
-
-  pub fn add(&self, other: &Tuple) -> Tuple {
-    Tuple {
-      x: self.x + other.x,
-      y: self.y + other.y,
-      z: self.z + other.z,
-      w: self.w + other.w
+    pub fn is_vector(&self) -> bool {
+        compare::equal(self.w, 0.0)
     }
-  }
 
-  pub fn sub(&self, other: &Tuple) -> Tuple {
-    Tuple {
-      x: self.x - other.x,
-      y: self.y - other.y,
-      z: self.z - other.z,
-      w: self.w - other.w
+    pub fn is_point(&self) -> bool {
+        compare::equal(self.w, 1.0)
     }
-  }
 
-  pub fn negate(&mut self) -> &Tuple {
-    self.x *= -1.0;
-    self.y *= -1.0;
-    self.z *= -1.0;
-    self.w *= -1.0;
-    self
-  }
+    pub fn magnitude(&self) -> f64 {
+        f64::sqrt(
+          self.x * self.x +
+          self.y * self.y +
+          self.z * self.z +
+          self.w * self.w
+        )
+    }
 
-  pub fn scale(&mut self, scale: f64) -> &Tuple {
-    self.x *= scale;
-    self.y *= scale;
-    self.z *= scale;
-    self.w *= scale;
-    self
-  }
-
-  pub fn divide(&mut self, divisor: f64) -> &Tuple {
-    self.x /= divisor;
-    self.y /= divisor;
-    self.z /= divisor;
-    self.w /= divisor;
-    self
-  }
-
-  pub fn magnitude(&self) -> f64 {
-    f64::sqrt(
-      self.x * self.x +
-      self.y * self.y +
-      self.z * self.z +
-      self.w * self.w
-    )
-  }
-
-  pub fn equals(&self, other: &Tuple) -> bool {
-    compare::equal(self.x, other.x) &&
-    compare::equal(self.y, other.y) &&
-    compare::equal(self.z, other.z) &&
-    compare::equal(self.w, other.w)
-  }
+    pub fn equals(&self, other: &Tuple) -> bool {
+        compare::equal(self.x, other.x) &&
+        compare::equal(self.y, other.y) &&
+        compare::equal(self.z, other.z) &&
+        compare::equal(self.w, other.w)
+    }
 }
 
 #[cfg(test)]
@@ -135,7 +148,7 @@ mod tests {
   fn add_tuples() {
     let x = Tuple::new_vector(3.0, -2.0, 5.0);
     let y = Tuple::new_point(-2.0, 3.0, 1.0);
-    let result = x.add(&y);
+    let result = x + y;
     let expected = Tuple::new_tuple(1.0, 1.0, 6.0, 1.0);
     assert_eq!(result.equals(&expected), true);
   }
@@ -144,7 +157,7 @@ mod tests {
   fn point_minus_vector() {
     let x = Tuple::new_point(3.0, 2.0, 1.0);
     let y = Tuple::new_vector(5.0, 6.0, 7.0);
-    let result = x.sub(&y);
+    let result = x - y;
     let expected = Tuple::new_point(-2.0, -4.0, -6.0);
     assert_eq!(result.equals(&expected), true);
   }
@@ -153,30 +166,32 @@ mod tests {
   fn vector_minus_vector() {
     let x = Tuple::new_vector(3.0, 2.0, 1.0);
     let y = Tuple::new_vector(5.0, 6.0, 7.0);
-    let result = x.sub(&y);
+    let result = x - y;
     let expected = Tuple::new_vector(-2.0, -4.0, -6.0);
     assert_eq!(result.equals(&expected), true);
   }
 
   #[test]
   fn negate_tuple() {
-    let mut x = Tuple::new_tuple(1.0, -2.0, 3.0, -4.0);
+    let x = -Tuple::new_tuple(1.0, -2.0, 3.0, -4.0);
     let expect = Tuple::new_tuple(-1.0, 2.0, -3.0, 4.0);
-    assert_eq!(x.negate().equals(&expect), true);
+    assert_eq!(x.equals(&expect), true);
   }
 
   #[test]
   fn scale_tuple() {
     let mut x = Tuple::new_tuple(1.0, -2.0, 3.0, -4.0);
+    x *= 3.5;
     let expect = Tuple::new_tuple(3.5, -7.0, 10.5, -14.0);
-    assert_eq!(x.scale(3.5).equals(&expect), true);
+    assert_eq!(x.equals(&expect), true);
   }
 
   #[test]
   fn divide_tuple() {
     let mut x = Tuple::new_tuple(1.0, -2.0, 3.0, -4.0);
+    x /= 2.0;
     let expect = Tuple::new_tuple(0.5, -1.0, 1.5, -2.0);
-    assert_eq!(x.divide(2.0).equals(&expect), true);
+    assert_eq!(x.equals(&expect), true);
   }
 
   #[test]
@@ -208,5 +223,4 @@ mod tests {
     let x = Tuple::new_vector(-1.0, -2.0, -3.0);
     assert_eq!(x.magnitude(), f64::sqrt(14.0));
   }
-
 }
